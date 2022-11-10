6b83fce7bb1ff429b46ab6c723ea7a42fe1fb799
Thomas Tsangou <63421640+M4G1C14N5@users.noreply.github.com>
2022-11-10 01:09:59 -0500

    Update javascript-methods.js

--- javascript-methods.js
+++ javascript-methods.js
@@ -10,7 +10,11 @@ In this Assignment, we use the prototype constructor to add new methods to the A
 
 // MAP //
 Array.prototype.myMap = function(callbackFn) {
-  // Place your code here.
+  let copy = [];
+  for (let i = 0; i < this.length; index++){
+    copy.push(callbackFn(this[i], i, this));
+  }
+  return resultArray;
 };
 
 // FILTER //
